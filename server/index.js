185c66708fdcc4fb24e11f10daca3110142968ee
gonzaloescobar <gonzalo.david.escobar@gmail.com>
2017-07-14 00:37:46 -0300

    New Feature: Welcome message

--- server/index.js
+++ server/index.js
@@ -3,8 +3,25 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static('client'));
+
 app.get('/hello-world', function(req, res){
   res.status(200).send('Hola mundo desde una ruta');
+});
+
+var messages = [{id: 1, text: 'Welcome to the private chat of Socket.io and NodeJs of Gonza Escobar...', nickname: 'Bot - gonzaescobar'}];
+
+io.on('connection', function(socket){
+    console.log("The node with IP: "+socket.handshake.address+" is connected...");
+
+    socket.emit('messages', messages);
+
+    socket.on('add-message', function(data){
+      messages.push(data);
+
+      io.sockets.emit('messages', messages);
+    });
+
 });
 
 server.listen(6677, function(){
